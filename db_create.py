12ac270e53229d4ff6a9fdde7db5db5fcef76e19
Edward Feng <edward0414@gmail.com>
2017-10-23 03:04:55 -0400

    Fixed Typo

--- db_create.py
+++ db_create.py
@@ -13,15 +13,15 @@ db.session.add(Transaction("Vancouver","2017-10-22", "Edward Feng", "Edward Feng
 db.session.add(Transaction("Administration", "2017-10-22", "Edward Feng", "Edward Feng", "Initial", 0, 0))
 db.session.add(Staff("Edward", "Feng", "admin@admin", "admin", "Senior", "All", "Treasurer", "University of Toronto"))
 db.session.add(MoneyCount("Income", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1))
-db.session.add(MoneyCount("expense", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1))
+db.session.add(MoneyCount("Expense", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1))
 db.session.add(MoneyCount("Income", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2))
-db.session.add(MoneyCount("expense", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2))
+db.session.add(MoneyCount("Expense", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2))
 db.session.add(MoneyCount("Income", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3))
-db.session.add(MoneyCount("expense", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3))
+db.session.add(MoneyCount("Expense", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3))
 db.session.add(MoneyCount("Income", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4))
-db.session.add(MoneyCount("expense", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4))
+db.session.add(MoneyCount("Expense", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4))
 db.session.add(MoneyCount("Income", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5))
-db.session.add(MoneyCount("expense", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5))
+db.session.add(MoneyCount("Expense", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5))
 
 
 # commit the changes
